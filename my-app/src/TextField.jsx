98141993a98105b5c991dc985163265c290138e2
Anjan <anjan.behera123@gmail.com>
2020-03-04 10:22:53 +0530

    remove some unnecessary files

--- my-app/src/TextField.jsx
+++ my-app/src/TextField.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { string } from 'prop-types';
 
 const TextField = props => <div className="textfield f4">{props.text}</div>
+
+TextField.propTypes = {
+    text: string.isRequired
+};
+
 export default TextField;
